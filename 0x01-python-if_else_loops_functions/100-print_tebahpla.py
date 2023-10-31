4caf4789a66588758c0a0a0ae71a28dcfdd6ed21
Mutali09 <nelsonsimiyu9@gmail.com>
2023-10-31 07:21:46 -0700

    100-print_tebahpla.py

---
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+for char_code in range(122, 96, -1):
+    if char_code % 2 == 0:
+        print("{:c}".format(char_code - 32), end="")
+    else:
+        print("{:c}".format(char_code), end="")
