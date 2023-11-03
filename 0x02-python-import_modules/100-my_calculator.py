61a29921d20bf7b1b322921eeede19ebdcddb67c
Mutali09 <nelsonsimiyu9@gmail.com>
2023-11-02 22:35:48 -0700

    100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -5,15 +5,19 @@ if __name__ == "__main__":
     if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
-        a, op, b = map(int, sys.argv[1:4])
-        if op not in ('+', '-', '*', '/'):
-            print("Unknown operator. Available operators: +, -, *, and /")
-            sys.exit(1)
-            operators = {
-                    '+': add(a, b),
-                    '-': add(a, b),
-                    '*': add(a, b),
-                    '/': add(a, b),
-                    }
-            print(f"{a} {op} {b} = {operators[op]}")
-
+    a, op, b = sys.argv[1:4]
+    if op not in ('+', '-', '*', '/'):
+         print("Unknown operator. Available operators: +, -, *, and /")
+         sys.exit(1)
+    a, b = int(a), int(b)     
+    operators = {
+             '+': add,
+             '-': sub,
+             '*': mul,
+             '/': div,
+             }
+    if b == 0 and (op in ('/', '%')):
+         print("Error: dividion by zero")
+         sys.exit(1)
+    result = operators[op](a, b)
+    print(f"{a} {op} {b} = {result}")
