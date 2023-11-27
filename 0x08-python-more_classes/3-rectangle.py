4eeaa520131fc9c7a7d2ce7607135178f65be6db
Mutali09 <nelsonsimiyu9@gmail.com>
2023-11-27 10:23:37 -0800

    task 3 edited

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -3,9 +3,11 @@ class Rectangle:
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
+
     @property
     def width(self):
         return self.__width
+
     @width.setter
     def width(self, value):
         if not isinstance(value, int):
@@ -13,9 +15,11 @@ class Rectangle:
         elif value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
+
     @property
     def height(self):
         return self.__height
+
     @height.setter
     def height(self, value):
         if not isinstance(value, int):
@@ -23,10 +27,14 @@ class Rectangle:
         elif value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
+
     def area(self):
         return self.width * self.height
+
     def perimeter(self):
-        return 2 * (self.width + self.height) if self.width != 0 and self.height != 0 else 0
+        return 2 * (self.width + self.height) if self.width != 0
+    and self.height != 0 else 0
+
     def __str__(self):
         result = ""
         if self.width == 0 or self.height == 0:
@@ -35,4 +43,5 @@ class Rectangle:
             result += '#' * self.width
             if i < self.height - 1:
                 result += '\n'
-        return (result)        
+
+                return (result)
