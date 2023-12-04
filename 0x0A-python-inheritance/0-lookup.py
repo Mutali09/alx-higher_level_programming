0de5fecdfb1dff52bd444b3053b0240784fbc66c
Mutali09 <nelsonsimiyu9@gmail.com>
2023-12-04 09:37:52 -0800

    0x0A. Python - Inheritance

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Function that defines an object attribute lookup."""
+
+
+def lookup(obj):
+    """Return a list object."""
+    return (dir(obj))
---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def is_kind_of_class(obj, a_class):
+    """Check if an object is an instance or inherited instance of a class.
+
+    Args:
+        obj (any): The object to check.
+        a_class (type): The class to match the type of obj to.
+    Returns:
+        True if obj is an instance or inherited instance of a_class, otherwise False.
+    """
+    return isinstance(obj, a_class)
