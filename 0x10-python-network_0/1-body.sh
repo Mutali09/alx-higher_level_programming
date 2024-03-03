78f77d8c7dcfb276975d2225979d998ac25df40b
Mutali09 <nelsonsimiyu9@gmail.com>
2024-03-02 21:37:20 -0800

    1-body.sh

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# This script takes a URL, sends a GET request using curl, and displays the body of the response if the status code is 200.
 
 # Check if the user has provided a URL as an argument
 if [ $# -ne 1 ]; then
@@ -10,20 +11,16 @@ fi
 url=$1
 
 # Send a GET request to the URL and store the response body
-response=$(curl -s -o temp.txt -w "%{http_code}" $url)
+response=$(curl -s -w "%{http_code}" $url)
 
 # Extract the status code from the response
-status_code=$(tail -n1 temp.txt)
-body=""
+status_code=$(tail -c 3 <<< "$response")
+body=$(head -c -3 <<< "$response")
 
 # Check if the status code is 200 (OK)
 if [ $status_code -eq 200 ]; then
     # Display the body of the response
-    body=$(sed '$d' temp.txt)
     echo "$body"
 else
     echo "Error: HTTP status code $status_code"
 fi
-
-# Clean up temporary file
-rm -f temp.txt
